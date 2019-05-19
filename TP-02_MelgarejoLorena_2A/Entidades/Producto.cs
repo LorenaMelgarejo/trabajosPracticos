f5fe84f233b98822b1ae1d45de43fe6d40b05f38
LorenaMelgarejo <lorena_melgarejo@yahoo.com.ar>
2019-05-19 16:58:41 -0300

    Add files via upload

---
+++ TP-02_MelgarejoLorena_2A/Entidades/Producto.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades_2018
+{
+    /// <summary>
+    /// La clase Producto no deberá permitir que se instancien elementos de este tipo.
+    /// </summary>
+    // sealed class Producto 
+    public abstract class Producto //(se cambio a abstract, ya que una clase sealed no permite la derivacion)
+    {
+        public enum EMarca // falto la propiedad pública
+        {
+            Serenisima, Campagnola, Arcor, Ilolay, Sancor, Pepsico
+        }
+
+        private EMarca marca;
+        private string codigoDeBarras;
+        private ConsoleColor colorPrimarioEmpaque;
+
+        /// <summary>
+        /// ReadOnly: Retornará la cantidad de ruedas del vehículo
+        /// </summary>
+
+        protected abstract short CantidadCalorias { get ;  }// solo retorna
+
+
+        public Producto(string codigo, EMarca marca, ConsoleColor color)
+        {
+            this.codigoDeBarras = codigo;
+            this.marca = marca;
+            this.colorPrimarioEmpaque = color;
+        }
+
+        /// <summary>
+        /// Publica todos los datos del Producto.
+        /// </summary>
+        /// <returns></returns>
+        public virtual string Mostrar() // se cambio la visibilidad a public  
+        { 
+            return (string)this;
+        }
+
+        public static explicit operator string(Producto p) //se paso la visibilidad a public 
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CODIGO DE BARRAS: " + p.codigoDeBarras); //se cambio la coma por el signo +
+            sb.AppendLine("MARCA          :  " + p.marca.ToString());
+            sb.AppendLine("COLOR EMPAQUE  :  " + p.colorPrimarioEmpaque.ToString());
+            sb.AppendLine("---------------------");
+
+            return sb.ToString(); //falto agregar el método ToString
+        }
+
+        /// <summary>
+        /// Dos productos son iguales si comparten el mismo código de barras
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator ==(Producto v1, Producto v2)
+        {
+            return (v1.codigoDeBarras == v2.codigoDeBarras);
+        }
+        /// <summary>
+        /// Dos productos son distintos si su código de barras es distinto
+        /// </summary>
+        /// <param name="v1"></param>
+        /// <param name="v2"></param>
+        /// <returns></returns>
+        public static bool operator !=(Producto v1, Producto v2)
+        {
+            return !(v1.codigoDeBarras == v2.codigoDeBarras);  //falto indicar el distinto (!)
+        }
+    }
+}
